83879ad32caa54bbee5f8516be554c4d94dec526
mathis diallo-themista <mathisdiallo-themista@mbp-de-mathis.home>
2023-10-15 20:39:36 +0200

    correction

--- 01_beluga_game/start.sql
+++ 01_beluga_game/start.sql
@@ -1,24 +1,23 @@
 CREATE TABLE beluga_game_entity
 (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
     hp INT,
     mp INT,
-    type VARCHAR(255) NOT NULL,
+    type VARCHAR(255),
     meta JSON,
     creation_date TIMESTAMP,
     last_action TIMESTAMP,
-    last_ip_use VARCHAR(255) NOT NULL,
+    last_ip_use VARCHAR(255),
     weapon_used INT,
-    CONSTRAINT fk_weapon_used
     FOREIGN KEY (weapon_used) REFERENCES beluga_game_weapon(id)
 );
 
 CREATE TABLE beluga_game_weapon
 (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    type VARCHAR(255) NOT NULL,
+    id AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    type VARCHAR(255),
     meta JSON,
     creation_date TIMESTAMP,
     attack INT,
@@ -27,13 +26,10 @@ CREATE TABLE beluga_game_weapon
 
 CREATE TABLE beluga_game_session
 (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    type VARCHAR(255) NOT NULL,
+    id AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
     meta JSON,
-    creation_date DATETIME,
-    creation_date DEFAULT,
-    creation_date CURRENT_TIMESTAMP,
-    last_action DATETIME,
+    creation_date TIMESTAMP,
+    last_action TIMESTAMP,
     list_entity JSON
 );
