8b343238cf2d5da53f0619c875196babb0b78767
mathis diallo-themista <mathisdiallo-themista@mbp-de-mathis.home>
2023-10-15 20:23:29 +0200

    correction moulinette

--- 00_beluga_com/create.sql
+++ 00_beluga_com/create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE company (
-    company_id INT AUTO_INCREMENT PRIMARY KEY,
+    company_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    id VARCHAR(255),
+    id VARCHAR(255) NOT NULL,
     description TEXT
 );
